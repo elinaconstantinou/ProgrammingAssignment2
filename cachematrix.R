6d4046854af94eedb60053230db8e6dabbd2163f
Constantinou <Elina.Constantinou@kpmg.com.cy>
2022-08-07 15:10:08 +0300

    Coursera programming Assignmet 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  m <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  
+  setinverse <- function(inverse) m <<- inverse
 
+  getinverse <- function() m
+  
+  list(set = set, 
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data,...)
+  x$setinverse(m)
+  m
 }
+
+################### Example to check code
+
+A <- matrix( c(5, 1, 0,
+               3,-1, 2,
+               4, 0,-1), nrow=3, byrow=TRUE)
+
+aMatrix <- makeCacheMatrix(A)
+aMatrix$get()               # retrieve the value of matrix
+aMatrix$getinverse()        # retrieve the value of m, which should be NULL before anything is cached
+cacheSolve(aMatrix)         # inverse calculated 
+aMatrix$getinverse()        # retrieve it directly, now that it has been cached
+cacheSolve(aMatrix)         # test the "getting cached data"
